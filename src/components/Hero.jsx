eb776fe1a46e3c16d967a79726a8c619c0ca5a02
Christopher Amah <christopheramah0@gmail.com>
2025-09-16 15:55:20 +0100

    final

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,14 +31,14 @@ const HeroSection = () => {
             
             {/* Store Buttons */}
             <div className="mt-10 flex flex-col sm:flex-row lg:items-start gap-4">
-              <a href="#" className="flex items-center gap-3 bg-[#01011C]  border border-[#A6A6A6] text-white rounded-lg px-3 py-2 md:px-5 md:py-3 w-40 transition-transform duration-300 hover:scale-105">
+              <a href="#" className="flex items-center gap-3 bg-[#01011C]  border border-[#A6A6A6] text-white rounded-lg px-3 py-2 md:px-5 md:py-3 w-40 md:w-48 transition-transform duration-300 hover:scale-105">
                 <FaApple className="w-5 h-5 md:w-7 md:h-7"/>
                 <div>
                   <p className="text-xs font-medium">Download on the</p>
                   <p className="text-sm md:text-xl md:font-semibold">App Store</p>
                 </div>
               </a>
-              <a href="#" className="flex items-center gap-3 bg-[#01011C]  border border-[#A6A6A6] text-white rounded-lg px-3 py-2 md:px-5 md:py-3 w-40 transition-transform duration-300 hover:scale-105">
+              <a href="#" className="flex items-center gap-3 bg-[#01011C]  border border-[#A6A6A6] text-white rounded-lg px-3 py-2 md:px-5 md:py-3 w-40 md:w-48 transition-transform duration-300 hover:scale-105">
                 <img src={playstore} alt="Playstore" className="w-4 h-4 md:w-7 md:h-7"/>
                 <div>
                   <p className="text-xs font-medium">GET IT ON</p>
