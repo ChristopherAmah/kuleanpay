c57089e90e6ed30fc68d983d30a60c582ed3b8ad
Christopher Amah <christopheramah0@gmail.com>
2025-09-16 15:53:07 +0100

    final

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
           
           {/* Left Column: Text */}
           <div className="order-1 lg:order-1 lg:pl-10 px-6 lg:px-0">
-            <h1 className="text-4xl lg:text-7xl font-bold font-satoshi pt-6 md:pt-0 text-[#1B2632]">
+            <h1 className="text-4xl lg:text-7xl font-bold font-satoshi pt-20 md:pt-0 text-[#1B2632]">
               Unlock Better Finances with Kuleanpay!
             </h1>
             <p className="mt-[16px] px-2 font-satoshi md:text-[18px] text-[#19203199] md:max-w-xl">
@@ -31,18 +31,18 @@ const HeroSection = () => {
             
             {/* Store Buttons */}
             <div className="mt-10 flex flex-col sm:flex-row lg:items-start gap-4">
-              <a href="#" className="flex items-center gap-3 bg-[#01011C] shadow-lg border border-[#A6A6A6] text-white rounded-lg px-5 py-3 w-48 transition-transform duration-300 hover:scale-105">
-                <FaApple className="w-7 h-7"/>
+              <a href="#" className="flex items-center gap-3 bg-[#01011C]  border border-[#A6A6A6] text-white rounded-lg px-3 py-2 md:px-5 md:py-3 w-40 transition-transform duration-300 hover:scale-105">
+                <FaApple className="w-5 h-5 md:w-7 md:h-7"/>
                 <div>
                   <p className="text-xs font-medium">Download on the</p>
-                  <p className="md:text-xl font-semibold">App Store</p>
+                  <p className="text-sm md:text-xl md:font-semibold">App Store</p>
                 </div>
               </a>
-              <a href="#" className="flex items-center gap-3 bg-[#01011C] shadow-lg border border-[#A6A6A6] text-white rounded-lg px-5 py-3 w-48 transition-transform duration-300 hover:scale-105">
-                <img src={playstore} alt="Playstore" className="w-6 h-6"/>
+              <a href="#" className="flex items-center gap-3 bg-[#01011C]  border border-[#A6A6A6] text-white rounded-lg px-3 py-2 md:px-5 md:py-3 w-40 transition-transform duration-300 hover:scale-105">
+                <img src={playstore} alt="Playstore" className="w-4 h-4 md:w-7 md:h-7"/>
                 <div>
                   <p className="text-xs font-medium">GET IT ON</p>
-                  <p className="md:text-lg font-semibold">Google Play</p>
+                  <p className="text-sm md:text-lg md:font-semibold">Google Play</p>
                 </div>
               </a>
             </div>
